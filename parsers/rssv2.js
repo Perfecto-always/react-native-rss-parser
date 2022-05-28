ec9b57e09f992ed2c45d9c06e94591f94c25f57a
Perfecto-always <harsh24bararia@gmail.com>
2022-05-28 15:15:51 +0530

    Added media Object in items array

--- parsers/rssv2.js
+++ parsers/rssv2.js
@@ -6,170 +6,179 @@ const itunesParser = require('./itunes');
 const getChannelTitle = (node) => utils.getElementTextContent(node, 'title');
 
 const getChannelLinks = (node) => {
-  const links = utils.getChildElements(node, 'link');
+    const links = utils.getChildElements(node, 'link');
 
-  return links.map((link) => ({
-    url: link.textContent,
-    rel: link.getAttribute('rel'),
-  }));
+    return links.map((link) => ({
+        url: link.textContent,
+        rel: link.getAttribute('rel'),
+    }));
 };
 
 const getChannelDescription = (node) =>
-  utils.getElementTextContent(node, 'description');
+    utils.getElementTextContent(node, 'description');
 
 const getChannelLanguage = (node) =>
-  utils.getElementTextContent(node, 'language');
+    utils.getElementTextContent(node, 'language');
 
 const getChannelCopyright = (node) =>
-  utils.getElementTextContent(node, 'copyright');
+    utils.getElementTextContent(node, 'copyright');
 
 const getChannelAuthors = (node) => {
-  const authors = utils.getElementTextContentArray(node, 'managingEditor');
+    const authors = utils.getElementTextContentArray(node, 'managingEditor');
 
-  return authors.map((author) => ({
-    name: author,
-  }));
+    return authors.map((author) => ({
+        name: author,
+    }));
 };
 
 const getChannelLastUpdated = (node) =>
-  utils.getElementTextContent(node, 'lastBuildDate');
+    utils.getElementTextContent(node, 'lastBuildDate');
 
 const getChannelLastPublished = (node) =>
-  utils.getElementTextContent(node, 'pubDate');
+    utils.getElementTextContent(node, 'pubDate');
 
 const getChannelCategories = (node) => {
-  const categories = utils.getElementTextContentArray(node, 'category');
+    const categories = utils.getElementTextContentArray(node, 'category');
 
-  return categories.map((category) => ({
-    name: category,
-  }));
+    return categories.map((category) => ({
+        name: category,
+    }));
 };
 
 const getChannelImage = (node) => {
-  const imageNodes = utils.getChildElements(node, 'image');
+    const imageNodes = utils.getChildElements(node, 'image');
 
-  if (imageNodes.length === 0) {
+    if (imageNodes.length === 0) {
+        return {
+            url: undefined,
+            title: undefined,
+            description: undefined,
+            width: undefined,
+            height: undefined,
+        };
+    }
+
+    const imageNode = imageNodes[0];
+
     return {
-      url: undefined,
-      title: undefined,
-      description: undefined,
-      width: undefined,
-      height: undefined,
+        url: utils.getElementTextContent(imageNode, 'url'),
+        title: utils.getElementTextContent(imageNode, 'title'),
+        description: utils.getElementTextContent(imageNode, 'description'),
+        width: utils.getElementTextContent(imageNode, 'width'),
+        height: utils.getElementTextContent(imageNode, 'height'),
     };
-  }
-
-  const imageNode = imageNodes[0];
-
-  return {
-    url: utils.getElementTextContent(imageNode, 'url'),
-    title: utils.getElementTextContent(imageNode, 'title'),
-    description: utils.getElementTextContent(imageNode, 'description'),
-    width: utils.getElementTextContent(imageNode, 'width'),
-    height: utils.getElementTextContent(imageNode, 'height'),
-  };
 };
 
 const getItemTitle = (node) => utils.getElementTextContent(node, 'title');
 
 const getItemLinks = (node) => {
-  const links = utils.getChildElements(node, 'link');
+    const links = utils.getChildElements(node, 'link');
 
-  return links.map((link) => ({
-    url: link.textContent,
-    rel: link.getAttribute('rel'),
-  }));
+    return links.map((link) => ({
+        url: link.textContent,
+        rel: link.getAttribute('rel'),
+    }));
 };
 
 const getItemDescription = (node) =>
-  utils.getElementTextContent(node, 'description');
+    utils.getElementTextContent(node, 'description');
 
 const getItemContent = (node) =>
-  utils.getElementTextContent(node, 'encoded', namespaces.content);
+    utils.getElementTextContent(node, 'encoded', namespaces.content);
 
 const getItemAuthors = (node) => {
-  let authors = utils.getElementTextContentArray(node, 'author');
+    let authors = utils.getElementTextContentArray(node, 'author');
 
-  if (authors.length === 0) {
-    authors = utils.getElementTextContentArray(node, 'dc:creator');
-  }
+    if (authors.length === 0) {
+        authors = utils.getElementTextContentArray(node, 'dc:creator');
+    }
 
-  return authors.map((author) => ({
-    name: author,
-  }));
+    return authors.map((author) => ({
+        name: author,
+    }));
 };
 
 const getItemCategories = (node) => {
-  let categories = utils.getElementTextContentArray(node, 'category');
+    let categories = utils.getElementTextContentArray(node, 'category');
 
-  if (categories.length === 0) {
-    categories = utils.getElementTextContentArray(node, 'dc:subject');
-  }
+    if (categories.length === 0) {
+        categories = utils.getElementTextContentArray(node, 'dc:subject');
+    }
 
-  return categories.map((category) => ({
-    name: category,
-  }));
+    return categories.map((category) => ({
+        name: category,
+    }));
 };
 
 const getItemId = (node) => utils.getElementTextContent(node, 'guid');
 
 const getItemPublished = (node) =>
-  utils.getElementTextContent(node, 'pubDate') ||
-  utils.getElementTextContent(node, 'dc:date');
+    utils.getElementTextContent(node, 'pubDate') ||
+    utils.getElementTextContent(node, 'dc:date');
 
 const getItemEnclosures = (node) => {
-  const enclosures = utils.getChildElements(node, 'enclosure');
+    const enclosures = utils.getChildElements(node, 'enclosure');
 
-  return enclosures.map((enclosure) => ({
-    url: enclosure.getAttribute('url'),
-    length: enclosure.getAttribute('length'),
-    mimeType: enclosure.getAttribute('type'),
-  }));
+    return enclosures.map((enclosure) => ({
+        url: enclosure.getAttribute('url'),
+        length: enclosure.getAttribute('length'),
+        mimeType: enclosure.getAttribute('type'),
+    }));
+};
+
+const getItemMedia = (node) => {
+    return {
+        content: utils.getElementTextContent(node, 'media:content'),
+        credit: utils.getElementTextContent(node, 'media:credit'),
+        description: utils.getElementTextContent(node, 'media:description'),
+    };
 };
 
 const mapChannelFields = (document) => {
-  const channelNodes = utils.getElements(document, 'channel');
+    const channelNodes = utils.getElements(document, 'channel');
 
-  if (!channelNodes || channelNodes.length === 0) {
-    throw new Error('Could not find channel node');
-  }
+    if (!channelNodes || channelNodes.length === 0) {
+        throw new Error('Could not find channel node');
+    }
 
-  const channelNode = channelNodes[0];
+    const channelNode = channelNodes[0];
 
-  return {
-    title: getChannelTitle(channelNode),
-    links: getChannelLinks(channelNode),
-    description: getChannelDescription(channelNode),
-    language: getChannelLanguage(channelNode),
-    copyright: getChannelCopyright(channelNode),
-    authors: getChannelAuthors(channelNode),
-    lastUpdated: getChannelLastUpdated(channelNode),
-    lastPublished: getChannelLastPublished(channelNode),
-    categories: getChannelCategories(channelNode),
-    image: getChannelImage(channelNode),
-    itunes: itunesParser.parseChannel(channelNode),
-  };
+    return {
+        title: getChannelTitle(channelNode),
+        links: getChannelLinks(channelNode),
+        description: getChannelDescription(channelNode),
+        language: getChannelLanguage(channelNode),
+        copyright: getChannelCopyright(channelNode),
+        authors: getChannelAuthors(channelNode),
+        lastUpdated: getChannelLastUpdated(channelNode),
+        lastPublished: getChannelLastPublished(channelNode),
+        categories: getChannelCategories(channelNode),
+        image: getChannelImage(channelNode),
+        itunes: itunesParser.parseChannel(channelNode),
+    };
 };
 
 const mapItems = (document) => {
-  const itemNodes = utils.getElements(document, 'item');
+    const itemNodes = utils.getElements(document, 'item');
 
-  return itemNodes.map((item) => ({
-    title: getItemTitle(item),
-    links: getItemLinks(item),
-    description: getItemDescription(item),
-    content: getItemContent(item),
-    id: getItemId(item),
-    authors: getItemAuthors(item),
-    categories: getItemCategories(item),
-    published: getItemPublished(item),
-    enclosures: getItemEnclosures(item),
-    itunes: itunesParser.parseItem(item),
-  }));
+    return itemNodes.map((item) => ({
+        title: getItemTitle(item),
+        links: getItemLinks(item),
+        description: getItemDescription(item),
+        content: getItemContent(item),
+        id: getItemId(item),
+        authors: getItemAuthors(item),
+        categories: getItemCategories(item),
+        published: getItemPublished(item),
+        enclosures: getItemEnclosures(item),
+        itunes: itunesParser.parseItem(item),
+        media: getItemMedia(item),
+    }));
 };
 
 exports.parse = (document) => ({
-  ...model.rss,
-  type: 'rss-v2',
-  ...mapChannelFields(document),
-  items: mapItems(document),
+    ...model.rss,
+    type: 'rss-v2',
+    ...mapChannelFields(document),
+    items: mapItems(document),
 });
